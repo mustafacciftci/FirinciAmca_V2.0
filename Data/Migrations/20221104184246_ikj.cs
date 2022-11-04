498ab3a9d0c7cb3fd700632efe702671e7c75d52
musta <musta@DESKTOP-ICLIOQS>
2022-11-04 22:56:40 +0300

    Proje dosyası ekle.

---
+++ Data/Migrations/20221104184246_ikj.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FirinciAmca_V2._0.Data.Migrations
+{
+    public partial class ikj : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
